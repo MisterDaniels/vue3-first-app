a0b6b8a6487bc058006807a84317a26b6145a576
Daniel Silva <dodosilva51@gmail.com>
2020-09-16 08:23:28 -0300

    Bind styles and computed properties

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             cart: 0,
+            brand: 'Vue Mastery',
             product: {
                 name: 'Socks',
                 description: 'Is just comfortable socks with a Vue logo in it',
@@ -17,20 +18,35 @@ const app = Vue.createApp({
                     { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
                     { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
                 ]
-            }
+            },
+            selectedVariant: 0
         }
     },
     methods: {
         addToCart() {
             this.cart++;
         },
-        updateImage(variantImage) {
-            this.product.image = variantImage;
+        updateVariant(index) {
+            this.selectedVariant = index;
         },
         removeFromCart() {
             if (this.cart > 0) {
                 this.cart--;
             }
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product.name;
+        },
+        image() {
+            return this.product.variants[this.selectedVariant].image;
+        },
+        inStock() {
+            return this.product.inventory;
+        },
+        onSale() {
+            return this.product.onSale;
         }
     }
 });
